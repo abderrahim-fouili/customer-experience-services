06a41c41e90a61a2d084f67196dfbc44233de1c5
FOUILI ABDERRAHIM <fouili.abderahim@gmail.com>
2024-02-23 01:35:38 +0100

    Generate an empty project with initializr spring boot

---
+++ src/main/java/com/foundever/technicaltest/restapi/customerexperienceservices/CustomerExperienceServicesApplication.java
@@ -0,0 +1,13 @@
+package com.foundever.technicaltest.restapi.customerexperienceservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerExperienceServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerExperienceServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foundever/technicaltest/restapi/customerexperienceservices/CustomerExperienceServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foundever.technicaltest.restapi.customerexperienceservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerExperienceServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
