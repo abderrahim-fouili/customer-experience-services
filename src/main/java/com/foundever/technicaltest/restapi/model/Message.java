1444a7450de735ca513ece53bd6d386b575e06b5
FOUILI ABDERRAHIM <fouili.abderahim@gmail.com>
2024-02-24 03:42:15 +0100

    Add Entity "Message"

---
+++ src/main/java/com/foundever/technicaltest/restapi/model/Message.java
@@ -0,0 +1,23 @@
+package com.foundever.technicaltest.restapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Message")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String sender;
+    private String content;
+}
