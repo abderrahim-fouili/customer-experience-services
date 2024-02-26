7e7bfbfaad0ac23482f4fb83ccde16830d5ad9aa
FOUILI ABDERRAHIM <fouili.abderahim@gmail.com>
2024-02-26 11:19:32 +0100

    Remove unused import

--- src/main/java/com/foundever/technicaltest/restapi/model/Message.java
+++ src/main/java/com/foundever/technicaltest/restapi/model/Message.java
@@ -1,23 +1,44 @@
 package com.foundever.technicaltest.restapi.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.*;
 
-@Entity(name = "Message")
+import java.util.Date;
+import java.util.List;
+
+
+@Entity
+@Table(name = "message")
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Setter
+@Getter
 public class Message {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String sender;
-    private String content;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "message_id")
+        private Long messageId;
+
+        @ManyToOne
+        @JsonIgnoreProperties({"messages"})
+        @JoinColumn(name = "client_case_id")
+        private ClientCase clientCase;
+
+        @Column(name = "sender")
+        private String sender;
+
+        @Column(name = "content")
+        private String content;
+
+        @Column(name = "flag")
+        private String flag;
+
+        @Column(name = "created_at")
+        @Temporal(TemporalType.TIMESTAMP)
+        private Date createdAt;
+
 }
+
