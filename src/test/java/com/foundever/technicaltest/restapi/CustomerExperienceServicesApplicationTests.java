b50354c12ded0591966ad29e0977ebf381ae5e04
FOUILI ABDERRAHIM <fouili.abderahim@gmail.com>
2024-02-26 11:09:07 +0100

    Remove unused dependencies

--- src/test/java/com/foundever/technicaltest/restapi/CustomerExperienceServicesApplicationTests.java
+++ src/test/java/com/foundever/technicaltest/restapi/CustomerExperienceServicesApplicationTests.java
@@ -1,4 +1,4 @@
-package com.foundever.technicaltest.restapi.customerexperienceservices;
+package com.foundever.technicaltest.restapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
