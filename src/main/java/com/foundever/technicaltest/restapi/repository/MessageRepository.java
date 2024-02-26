8a9e1960770cefc0454cf1073f919e71fd247dff
FOUILI ABDERRAHIM <fouili.abderahim@gmail.com>
2024-02-26 11:11:33 +0100

    Add Repository "MessageRepository"

--- src/main/java/com/foundever/technicaltest/restapi/repository/MessageRepository.java
+++ src/main/java/com/foundever/technicaltest/restapi/repository/MessageRepository.java
@@ -4,6 +4,11 @@ import com.foundever.technicaltest.restapi.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    //Optional<Message> findBySender(String sender);
+    Optional<List<Message>> findBySender(String sender);
 }
