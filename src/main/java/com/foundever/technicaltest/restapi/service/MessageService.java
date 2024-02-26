ed2067a36fa5bde85b916783e7e90a9b6c67da6c
FOUILI ABDERRAHIM <fouili.abderahim@gmail.com>
2024-02-26 11:13:08 +0100

    Add MessageService and MessageServiceImp

--- src/main/java/com/foundever/technicaltest/restapi/service/MessageService.java
+++ src/main/java/com/foundever/technicaltest/restapi/service/MessageService.java
@@ -1,8 +1,12 @@
 package com.foundever.technicaltest.restapi.service;
 
-import com.foundever.technicaltest.restapi.model.ClientCase;
 import com.foundever.technicaltest.restapi.model.Message;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface MessageService extends CrudService<Long, Message> {
-    //Ajouter des methodes sur mesures;
+    //public Optional<Message> findMessageBySender(String sender);
+    public Optional<List<Message>> findMessageBySender(String sender);
+
 }
